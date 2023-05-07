94dd1212d548e02fefadd0e8261b8453c2744d19
masaharu0 <92728965+masaharu0@users.noreply.github.com>
2023-05-07 11:38:34 +0900

    Delete blackjack directory

--- blackjack/cards.rb
+++
@@ -1,8 +0,0 @@
-class Cards
-    def create_deck
-        suits = ['ハート', 'スペード', 'ダイヤ', 'クラブ']
-        ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-        deck = suits.product(ranks)
-        deck.shuffle!
-    end
-end
